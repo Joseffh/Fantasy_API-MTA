eb5e40b99f73475c91720f916d80a1a111970031
Spectro3n <151880712+Joseffh@users.noreply.github.com>
2025-08-03 00:01:36 -0300

    Delete luaC.lua

--- luaC.lua
+++
@@ -1,7 +0,0 @@
-addEvent( "Client-Side:PlayMusic", true)
-addEventHandler( "Client-Side:PlayMusic", root, function (player, url)
-    if not url or url == "" then return end
-    local x, y, z = getElementPosition(player)
-    playSound3D(url, x, y, z, true)
-    print("Tocando música: " .. url, player, 0, 255, 0)
-end)
